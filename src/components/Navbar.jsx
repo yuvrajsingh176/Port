280cb20b9a0797922e56728298da0e202aad11b2
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-01-30 21:14:17 +0530

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,44 @@
-
 import "./Navbar.css";
-import { FiHome } from  'react-icons/fi'
-import { BsFillPersonFill } from 'react-icons/bs'
-import { SiGooglescholar } from 'react-icons/si'
-import { PiProjectorScreenChartThin } from 'react-icons/pi'
-import { PiCertificateBold } from 'react-icons/pi'
-import {FcDocument} from 'react-icons/fc'
-import { GiHamburgerMenu } from "react-icons/gi"
-import {AiOutlineClose} from "react-icons/ai"
+import { FiHome } from "react-icons/fi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { SiGooglescholar } from "react-icons/si";
+import { PiProjectorScreenChartThin } from "react-icons/pi";
+import { PiCertificateBold } from "react-icons/pi";
+import { FcDocument } from "react-icons/fc";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 import { useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
-    const navRef = useRef();
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  const handleScroll = (elementId) => {
+    showNavbar();
+    const element = document.getElementById(elementId);
+
+    if (element) {
+      element.scrollIntoView({
+        behavior: "instant",
+      });
     }
+  };
 
-    const handleScroll=(elementId)=> {
-        showNavbar();
-  const element = document.getElementById(elementId);
-
-  if (element) {
-    element.scrollIntoView({
-      behavior: "instant",
-    });
-  }
-}
-
-
-
-    return (
-        <header>
-        <a href="/" className="d-flex navbar-brand" onClick={() => {
-                    handleScroll("/");
-                }}> 
-                    <h1 className="logo-text">yuvi</h1>
-                </a>
-            <nav ref={navRef}>
-                {/*</nav>  
+  return (
+    <header>
+      <a
+        href="/"
+        className="d-flex navbar-brand"
+        onClick={() => {
+          handleScroll("/");
+        }}
+      >
+        <h1 className="logo-text">yuvi</h1>
+      </a>
+      <nav ref={navRef}>
+        {/*</nav>  
                  <div className="navbar-collapse collapse">
                      <div className="ms-auto navbar-nav">
                          <div className="nav-item">
@@ -81,50 +82,65 @@ const Navbar = () => {
         
                 </div >
                             */}
-                            <div className="nav-item" >
-                            <a  className="nav-link" href="#home" onClick={() => {
-                                        handleScroll("home");
-                            }}>
-                                <FiHome/>
-                                <span className="intro">Home</span>
+        <div className="nav-item">
+          <a
+            className="nav-link"
+            href="#home"
+            onClick={() => {
+              handleScroll("home");
+            }}
+          >
+            <FiHome />
+            <span className="intro">Home</span>
+          </a>
+        </div>
+        <div className="nav-item">
+          <a
+            className="nav-link"
+            href="#about"
+            onClick={() => {
+              handleScroll("about");
+            }}
+          >
+            <BsFillPersonFill />
+            <span className="intro">About</span>
+          </a>
+        </div>
 
-                            </a>
-                        </div>
-                        <div className="nav-item" >
-                            <a  className="nav-link" href="#about" onClick={() => {
-                                handleScroll("about");
-                            }}>
-                                <BsFillPersonFill/>
-                                <span className="intro">About</span>
+        <div className="nav-item">
+          <a
+            className="nav-link"
+            href="#projects"
+            onClick={() => {
+              handleScroll("projects");
+            }}
+          >
+            <PiProjectorScreenChartThin />
+            <span className="intro">Projects</span>
+          </a>
+        </div>
 
-                            </a>
-                        </div>
-                       
-                        <div className="nav-item" >
-                            <a  className="nav-link" href="#projects" onClick={() => {
-                                handleScroll("projects");
-                            }}>
-                                <PiProjectorScreenChartThin/>
-                                <span className="intro">Projects</span>
-
-                            </a>
-                        </div>
-                 
-                        <div className="nav-item">
-                            <a className="nav-link" href="https://drive.google.com/file/d/1mvsOada_KpsQDIlu2Id-pJEVD8424ZK_/view">
-                                <FcDocument/>
-                                <span className="intro">Resume</span>
-
-                            </a>
-                        </div>
-                <button type="button" onClick={showNavbar} className="nav-btn nav-close-btn">
-                    <FaTimes/>
-                </button>
-            </nav>
-            <button type="button" onClick={showNavbar} className="nav-btn">
-                    <FaBars/>
-                </button>
-        </header>
-    )
-}
+        <div className="nav-item">
+          <a
+            className="nav-link"
+            href="https://drive.google.com/file/d/18Bttm8jT2SJHcoYmfVCVTZ51zhQ0gtIK/view?usp=sharing"
+          >
+            <FcDocument />
+            <span className="intro">Resume</span>
+          </a>
+        </div>
+        <button
+          type="button"
+          onClick={showNavbar}
+          className="nav-btn nav-close-btn"
+        >
+          <FaTimes />
+        </button>
+      </nav>
+      <button type="button" onClick={showNavbar} className="nav-btn">
+        <FaBars />
+      </button>
+    </header>
+  );
+};
 export default Navbar;
